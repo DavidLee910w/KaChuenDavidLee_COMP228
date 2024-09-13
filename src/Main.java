c510b688cb7cc6d80c108417626a53ba82e19d0b
DKCL1989 <devilee1989@gmail.com>
2024-09-12 22:23:05 -0400

    Lab1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Singer singer1 = new Singer();
+
+        singer1.setSingerID(1);
+        singer1.setSingerName("Sabrina Carpenter");
+        singer1.setSingerAddress("USA");
+        singer1.setDateOfBirth("11-May-1999");
+        singer1.setnumberOfAlbums(6);
+
+        Singer singer2 = new Singer();
+        singer2.setSingerAtOnce(2,"Taylor Swift","USA","13-Dec-1989",11);
+
+        //display
+        System.out.println("Singer Details of Singer: "+singer1.getSingerID());
+        System.out.println("Singer Name "+singer1.getSingerName());
+        System.out.println("Singer Address "+singer1.getSingerAddress());
+        System.out.println("Date of Birth "+singer1.getDateOfBirth());
+        System.out.println("Number of Albums "+singer1.getNumberOfAlbums());
+        System.out.println();
+
+        System.out.println("Singer Details of Singer: "+singer2.getSingerID());
+        System.out.println("Singer Name "+singer2.getSingerName());
+        System.out.println("Singer Address "+singer2.getSingerAddress());
+        System.out.println("Date of Birth "+singer2.getDateOfBirth());
+        System.out.println("Number of Albums "+singer2.getNumberOfAlbums());
+        System.out.println();
     }
 }
---
+++ src/Singer.java
@@ -0,0 +1,94 @@
+public class Singer {
+    private int singerID;
+    private String singerName;
+    private String singerAddress;
+    private String dateOfBirth;
+    private int numberOfAlbums;
+    //constructors
+    public Singer() {}
+
+    public Singer(int singerID) {
+        this.singerID = singerID;
+    }
+    public Singer(int singerID, String singerName)
+    {
+        this.singerID = singerID;
+        this.singerName = singerName;
+    }
+    public Singer(int singerID, String singerName, String singerAddress)
+    {
+        this.singerID = singerID;
+        this.singerName = singerName;
+        this.singerAddress = singerAddress;
+    }
+    public Singer(int singerID, String singerName, String singerAddress, String dateOfBirth)
+    {
+        this.singerID = singerID;
+        this.singerName = singerName;
+        this.singerAddress = singerAddress;
+        this.dateOfBirth = dateOfBirth;
+    }
+    public Singer(int singerID, String singerName, String singerAddress, String dateOfBirth, int numberOfAlbums)
+    {
+        this.singerID = singerID;
+        this.singerName = singerName;
+        this.singerAddress = singerAddress;
+        this.dateOfBirth = dateOfBirth;
+        this.numberOfAlbums = numberOfAlbums;
+    }
+
+
+
+    //method to set and get
+    public int getSingerID()
+    {
+        return singerID;
+    }
+
+    public String getSingerAddress()
+    {
+        return singerAddress;
+    }
+
+    public String getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+    public String getSingerName()
+    {
+        return singerName;
+    }
+
+    public int getNumberOfAlbums()
+    {
+        return numberOfAlbums;
+    }
+    public void setSingerID(int singerID)
+    {
+        this.singerID = singerID;
+    }
+    public void setSingerName(String singerName)
+    {
+        this.singerName = singerName;
+    }
+    public void setSingerAddress(String singerAddress)
+    {
+        this.singerAddress = singerAddress;
+    }
+    public void setDateOfBirth(String dateOfBirth)
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public void setnumberOfAlbums(int numberOfAlbums)
+    {
+        this.numberOfAlbums = numberOfAlbums;
+    }
+    public void setSingerAtOnce(int id, String name, String address, String dateOfBirth, int numberOfAlbums )
+    {
+        this.singerID = id;
+        this.singerName = name;
+        this.singerAddress = address;
+        this.dateOfBirth = dateOfBirth;
+        this.numberOfAlbums = numberOfAlbums;
+    }
+}
