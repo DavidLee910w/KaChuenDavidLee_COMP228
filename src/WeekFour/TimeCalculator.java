b969f28e224f800b8d1acfea39c6da32c6fb18b3
DKCL1989 <devilee1989@gmail.com>
2024-10-07 18:54:37 -0400

    Week 5

--- src/WeekFour/TimeCalculator.java
+++ src/WeekFour/TimeCalculator.java
@@ -1,24 +1,24 @@
 package WeekFour;
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-
 public class TimeCalculator {
-
+    // Method to get current local time using LocalDateTime
     public void getCurrentLocalDateTime() {
         LocalDateTime ldt = LocalDateTime.now();
-        System.out.println("Current Local Date Time:"+ldt);
+        System.out.println("Current LocalDateTime: " + ldt);
     }
-    public void getZonedLocalDateTime() {
-        ZonedDateTime zdt = ZonedDateTime.now();
-        System.out.println("Current Zoned Date Time:"+zdt);
-    }
-    public static void main(String[] args) {
 
-        System.out.println(LocalDate.now());
-        System.out.println(LocalDateTime.now());
+    // Method to get current time using ZonedDateTime
+    public void getCurrentZonedDateTime() {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println("Current ZonedDateTime: " + zdt);
+    }
+
+    public static void main(String[] args) {
         TimeCalculator tc = new TimeCalculator();
+
         tc.getCurrentLocalDateTime();
-        tc.getZonedLocalDateTime();
+        tc.getCurrentZonedDateTime();
     }
 }
